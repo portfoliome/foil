c9d71eac495211e53749012c77ad78086c41e759
pmart123 <philip.martin2007@gmail.com>
2016-10-02 19:43:49 -0400

    Parsers for basic python types primarily to efficiently cast for database ETL processes.

---
+++ tests/test_parsers.py
@@ -0,0 +1,90 @@
+import unittest
+
+from foil.parsers import (make_converters, parse_bool, passthrough,
+                          parse_float, parse_int, parse_numeric,
+                          parse_quoted_bool, parse_quoted_float,
+                          parse_quoted_int, parse_quoted_numeric,
+                          parse_broken_json)
+
+
+class TestTextParsers(unittest.TestCase):
+    def test_bool(self):
+        mock_data = [('true', True), ('false', False),
+                     ('1', True), ('0', False), ('', None)]
+
+        for input_expected in mock_data:
+            with self.subTest(input_expect=input_expected):
+                result = parse_bool(input_expected[0])
+                expected = input_expected[1]
+                self.assertEqual(expected, result)
+
+    def test_float(self):
+        expected = 3.412
+        result = parse_float('3.412')
+        self.assertEqual(result, expected)
+
+    def test_int(self):
+        expected = 3
+        result = parse_int('3')
+        self.assertEqual(expected, result)
+
+    def test_numeric_nan_none(self):
+        nulls = ['']
+
+        for val in nulls:
+            with self.subTest(val=val):
+                self.assertEqual(parse_numeric(float, val), None)
+
+
+class TestQuotedTextParsers(unittest.TestCase):
+    def test_bool(self):
+        mock_data = [('"1"', True), ('"0"', False), ('""', None), ('', None)]
+
+        for input_expected in mock_data:
+            with self.subTest(input_expect=input_expected):
+                result = parse_quoted_bool(input_expected[0])
+                expected = input_expected[1]
+                self.assertEqual(expected, result)
+
+    def test_float(self):
+        expected = 3.412
+        result = parse_quoted_float('"3.412"')
+        self.assertEqual(result, expected)
+
+    def test_int(self):
+        expected = 3
+        result = parse_quoted_int('"3"')
+        self.assertEqual(expected, result)
+
+    def test_numeric_nan(self):
+        nulls = ['""', '']
+
+        for val in nulls:
+            with self.subTest(val=val):
+                self.assertEqual(parse_quoted_numeric(float, val), None)
+
+
+class Klass:
+    pass
+
+
+class TestMakeConverters(unittest.TestCase):
+    def test_casting_functions(self):
+        datatype_names = ['ticker', 'shares', 'price', 'bought', 'custom']
+        inputted_types = [str, int, float, bool, Klass]
+        expected_types = [passthrough, parse_int, parse_float,
+                          parse_bool, Klass]
+        inputted = dict(zip(datatype_names, inputted_types))
+        expected = dict(zip(datatype_names, expected_types))
+        result = make_converters(inputted)
+        self.assertEqual(expected, result)
+
+
+class TestJSONParsers(unittest.TestCase):
+    def test_parse_broken_json(self):
+        broken_json = '{success:true}'
+
+        expected = {'success': True}
+        result = parse_broken_json(broken_json)
+
+        self.assertEqual(expected, result)
