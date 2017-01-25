ec2af0053c4342dbaacf3563e54435702405103f
pmart123 <philip.martin2007@gmail.com>
2017-01-24 22:53:15 -0600

    Serialize UTC aware datetime with zulu suffix.

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -3,6 +3,8 @@ import unittest
 import uuid
 from datetime import date, datetime
 
+import pytz
+
 from foil.serializers import json_serializer
 
 
@@ -31,6 +33,23 @@ class TestJSONSerializer(unittest.TestCase):
         result = self._serialize(dt)
 
         self.assertEquals(expected, result)
+
+    def test_serialize_utc_datetime(self):
+        dt = datetime(2015, 3, 13, 20, 33, 22, 567, tzinfo=pytz.UTC)
+
+        expected = '"{}Z"'.format(dt.strftime('%Y-%m-%dT%H:%M:%S.%f'))
+        result = self._serialize(dt)
+
+        self.assertEqual(expected, result)
+
+    def test_serialize_aware_datetime(self):
+        eastern_zone = pytz.timezone('US/Eastern')
+        dt = eastern_zone.localize(datetime(2015, 3, 13, 20, 33, 22, 567))
+
+        expected = '"2015-03-13T20:33:22.000567-04:00"'
+        result = self._serialize(dt)
+
+        self.assertEqual(expected, result)
 
     def _serialize(self, obj):
         return json.dumps(obj, default=json_serializer)
